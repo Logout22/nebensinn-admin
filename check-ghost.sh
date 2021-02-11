806091988f8de8a21cbddb515e8a9ae7f0e95107
Martin Unzner <martin.u@posteo.de>
2021-02-11 19:51:30 +0100

    set up Ghost availability check

--- check-ghost.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-if ! curl -I -q {{external_address}} 2>/dev/null | head -n1 | grep -q 200; then
-	echo "Error: unable to reach Ghost home page!" | mail -s "Book club Ghost instance" webmaster
-fi
